685864d8b89db28290bb2e2850339eaf4ddb700e
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2025-07-17 01:57:15 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from agents import Agent,Runner
+from agents.extensions.models.litellm_model import LitellmModel
+MODEL_NAME="gemini/gemini-2.0-flash"
+API_KEY=config("Gemini_API_KEY")
+assistant=Agent(
+    name="Testing_Agent" ,
+                instuctions="Your are a helpful assistint",
+                model=LitellmModel(model=MODEL_NAME,api_key=API_KEY),
+                api_key=API_KEY)
+output =await Runner.run(starting_agent=assistant, input="How are you?")
+print(output.final_output)
