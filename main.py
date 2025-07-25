3c1197ee57e3fb5fe15e4de1b146ffb8b90eed2d
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2025-07-26 00:33:03 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from agents import Agent,Runner
-from agents.extensions.models.litellm_model import LitellmModel
-MODEL_NAME="gemini/gemini-2.0-flash"
-API_KEY=config("Gemini_API_KEY")
-assistant=Agent(
-    name="Testing_Agent" ,
-                instuctions="Your are a helpful assistint",
-                model=LitellmModel(model=MODEL_NAME,api_key=API_KEY),
-                api_key=API_KEY)
-output =await Runner.run(starting_agent=assistant, input="How are you?")
-print(output.final_output)
-            
-         
+from decouple import config
+from agents import AsyncOpenAI, OpenAIChatCompletionsModel,Agent,Runner, set_tracing_disabled
+def main():
+    set_tracing_disabled(True)
+
+    key=config("GEMINI_API_KEY")
+    base_url=config("base_url")
+    gemini_client=AsyncOpenAI(api_key=key,base_url=base_url)
+    Model=OpenAIChatCompletionsModel(model="gemini-2.5-flash",openai_client=gemini_client)
+    agent=Agent(
+        name="Kaif shamim",
+        instructions="you are my helpfull assistant.",
+        model=Model
+    )
+    res=Runner.run_sync(starting_agent=agent,input="how ai is destroying our self thonking and self confidence of achieving any thing")
+    print(res.final_output)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
